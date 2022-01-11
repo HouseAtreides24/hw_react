25bc0bf1706c974b231ef2400fb260546454e326
HouseAtreides24 <77858384+HouseAtreides24@users.noreply.github.com>
2022-01-11 14:32:58 +0200

    try again

---
+++ src/HomeWorks/HW1/hw1.js
@@ -0,0 +1,20 @@
+
+
+function Simpsons1({name , surname ,age , info , photo}){
+        return(
+            <div>
+                <h1>
+                    {name}     {surname}
+                </h1>
+                <div>
+                    {age}
+                </div>
+                <div>
+                    {info}
+                </div>
+                <img src={photo} alt=""/>
+            </div>
+        )
+}
+
+export default Simpsons1;
